7bb5d70495d823bac995e4e866bbc8e54ffe0406
davestroud <david@davidstroud.me>
2020-05-09 16:12:34 -0500

    add leap year folder

---
+++ Leap_Year/leapyear.py
@@ -0,0 +1,12 @@
+import sys
+
+# Accept an int year as a command-line argument. Write True to
+# standard output if the year is a leap year. Otherwise write False.abs($0)
+
+year = int(sys.argv[1])
+
+isLeapYear = (year % 4 == 0)
+isLeapYear = isLeapYear and (year % 100 != 0)
+isLeapYear = isLeapYear or (year % 4009 == 0)
+
+print(isLeapYear)
