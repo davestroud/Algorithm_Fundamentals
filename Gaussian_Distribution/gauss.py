bdaf54b0e4d2c6cc39a288101c69ffa87cbce143
davestroud <david@davidstroud.me>
2020-05-10 10:32:20 -0500

    add pdf function

--- Gaussian_Distribution/gauss.py
+++ Gaussian_Distribution/gauss.py
@@ -1,5 +1,11 @@
 import sys
 import math
 
+# Return the value of the Gaussian probability function with mean 0.0
+# and standard deviation of 1.0 at the given x value.
+
 def phi(x):
     return math.exp(-x * x / 2.0) / math.sqrt(2.0 * math.pi)
+
+def pdf(x, mu=0.0, sigma=1.0):
+    return phi((x - mu) / sigma) / sigma
