ef0306e29dde0790104b853980b6aa8937ec341f
davestroud <david@davidstroud.me>
2020-05-10 16:29:51 -0500

    add Functions to file

---
+++ Functions/Main.py
@@ -0,0 +1,21 @@
+# Simple function
+
+def my_function(x):
+    return 5 * x
+
+# print(my_function(3))
+# print(my_function(5))
+# print(my_function(9))
+#-----------------------------------------------------------
+
+# Return value with list
+
+def my_list(food):
+    for x in food:
+        print(x)
+
+fruits = ['apple', 'banana', 'cherry']
+
+# print(my_list(fruits))
+
+#-----------------------------------------------------------
