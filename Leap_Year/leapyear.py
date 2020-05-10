60044ed92793c493423f9d5ae2126a20e04dc879
davestroud <david@davidstroud.me>
2020-05-10 14:51:04 -0500

    add notes to leapyear

--- Leap_Year/leapyear.py
+++ Leap_Year/leapyear.py
@@ -2,6 +2,18 @@
 # leapyear.py
 #-----------------------------------------------------------------------
 
+'''
+The following pseudocode determines whether a year is a leap year or a common
+year in the Gregorian calendar (and in the proleptic Gregorian calendar
+before 1582). The year variable being tested is the integer representing
+the number of the year in the Gregorian calendar.
+
+if (year is not divisible by 4) then (it is a common year)
+else if (year is not divisible by 100) then (it is a leap year)
+else if (year is not divisible by 400) then (it is a common year)
+else (it is a leap year)
+'''
+
 import sys
 
 # Accept an int year as a command-line argument. Write True to
@@ -11,7 +23,7 @@ year = int(sys.argv[1])
 
 isLeapYear = (year % 4 == 0)
 isLeapYear = isLeapYear and (year % 100 != 0)
-isLeapYear = isLeapYear or (year % 4009 == 0)
+isLeapYear = isLeapYear or (year % 400 == 0)
 
 print(isLeapYear)
 
