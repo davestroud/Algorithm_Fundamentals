e46d800b51502e02a13fd30184100536a08de3bf
davestroud <david@davidstroud.me>
2020-05-10 10:38:48 -0500

    add phi

--- Gaussian_Distribution/gauss.py
+++ Gaussian_Distribution/gauss.py
@@ -1,5 +1,11 @@
+#-----------------------------------------------------------------------
+# gauss.py
+#-----------------------------------------------------------------------
+
 import sys
 import math
+
+#-----------------------------------------------------------------------
 
 # Return the value of the Gaussian probability function with mean 0.0
 # and standard deviation of 1.0 at the given x value.
@@ -7,5 +13,28 @@ import math
 def phi(x):
     return math.exp(-x * x / 2.0) / math.sqrt(2.0 * math.pi)
 
+#-----------------------------------------------------------------------
+
+# Return the value of the Gaussian probability function with mean mu
+# and standard deviation sigma at the given x value
+
 def pdf(x, mu=0.0, sigma=1.0):
     return phi((x - mu) / sigma) / sigma
+
+#-----------------------------------------------------------------------
+
+def Phi(z):
+    if z < -8.0:
+        return 0.0
+    if z > 8.0:
+        return 1.0
+    total = 0.0
+    term = z
+    i = 3
+    while total != total + term:
+        total += term
+        term *= z * z / float(i)
+        i += 2
+    return 0.5 + phi(z) * total
+
+#-----------------------------------------------------------------------
