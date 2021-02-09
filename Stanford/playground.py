61971b2cdb344e6598aac13ce251e3fa4b19cc7d
davestroud <david@davidstroud.me>
2021-02-08 18:18:32 -0600

    add main function

--- Stanford/playground.py
+++ Stanford/playground.py
@@ -1,3 +1,20 @@
+from random import randint
+from timeit import repeat
+
+def run_sorting_algorithm(algorithm, array):
+	# Set up the context and prepare the call to the specified
+	# algorithm using the supplied array. Only import the 
+	# algorithm function if it's not the built-in 'sorted()'.
+	setup_code = f"from __main__ import {algorithm}"
+		if algorithm != "sorted" else""
+
+	stmt = f"{algorithm}({array})"
+
+	# Execute the code ten different times and return the time
+	# in seconds that each execution took
+	times = repor
+
+
 def bubble_sort(array):
 	n = len(array)
 
@@ -27,3 +44,12 @@ def bubble_sort(array):
 	if already_sorted:
 		break 
 	return array
+
+if __name__ == "__main__":
+	# Generate an array of 'ARRAY_LENGTH' items consisting
+	# of random integer values between 0 and 999
+	array = [randit(0, 1000) for i in range(ARRAY_LENGTH)]
+
+	# Call the function using the name of the sorting algoritm 
+	# and the array you just created
+	
