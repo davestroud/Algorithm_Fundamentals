cc45d840a8ea489b590386d35573024ac6d8ac62
davestroud <david@davidstroud.me>
2021-02-16 10:32:20 -0500

    add insertion sort

--- Python/Sorting/main.py
+++ Python/Sorting/main.py
@@ -51,6 +51,25 @@ def bubble_sort(array):
 
     return array
 
+def insertion_sort(array):
+    # Loop from the second element of the array until
+    # the last element
+    for i in range(1, len(array)):
+        # THis is the element we want to position in its
+        # correct place
+        key_item = array[i]
+        
+        j = i - 1
+        
+        # Run throught the list of item (the left
+        # portion of the array) and find the correct position
+        # of the element referenced by 'key_item'. Do this only
+        # if 'key_item is smaller than its adjacent values.
+        while j >= 0 and array[j] > key_item:
+            # Shift the value one position to the left
+            # and reposition j to point to the next element
+            # (from right to left)
+
 
 ARRAY_LENGTH = 10000
 
