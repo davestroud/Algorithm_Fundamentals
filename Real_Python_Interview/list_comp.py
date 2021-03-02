94226ce13ce33931d2a5859e407b040c44b90e21
davestroud <david@davidstroud.me>
2021-03-02 10:46:52 -0600

    add list comprehension for grid

--- Real_Python_Interview/list_comp.py
+++ Real_Python_Interview/list_comp.py
@@ -13,4 +13,28 @@ for num in lst:
     
 # 3rd method ~ list comprehension
 [square(num) for num in lst]
-    
+
+##################################################
+# Interview type problem using list comprehensions
+#################################################
+
+# Standard method
+grid = [[0, 0, 0],
+        [0, 0, 0]]
+
+num_rows = 2
+num_columns = 3
+
+grid = []
+
+for _ in range(num_rows):
+    curr_row = []
+    for _ in range(num_columns):
+        curr_row.append(0)
+    grid.append(curr_row)
+   
+grid
+
+# List comprehension
+grid1 = [[0 for _ in range(num_columns)] for _ in range(num_rows)]
+grid1
