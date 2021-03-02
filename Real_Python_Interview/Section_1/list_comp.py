434fa82911e4a26f03ba46a51dbd45ee4661dc0b
davestroud <david@davidstroud.me>
2021-03-02 11:39:11 -0600

    finish min max and any

--- Real_Python_Interview/Section_1/list_comp.py
+++ Real_Python_Interview/Section_1/list_comp.py
@@ -38,3 +38,17 @@ grid
 # List comprehension
 grid1 = [[0 for _ in range(num_columns)] for _ in range(num_rows)]
 grid1
+
+
+''' Max, min, any
+    lst = [1, 2, -5, 4]
+'''
+# Returns min and max square root
+max(lst, key=lambda x: x * x)
+min(lst, key=lambda x: x * x)
+
+# checks if any of the numbers in list are odd
+# note the any and all function don't take in a 
+# key value
+any([(lambda x: x % 2 ==1)(num) for num in lst])
+all([(lambda x: x % 2 ==1)(num)for num in lst])
