7b6eee09c790342049e55daa6f023485ffe9a581
davestroud <david@davidstroud.me>
2021-03-06 15:39:18 -0600

    add already sorted

--- Real_Python/Sorting/bubble_sort.py
+++ Real_Python/Sorting/bubble_sort.py
@@ -1,6 +1,10 @@
 def bubble_sort(items):
     for i in range(len(items)):
+        already_sorted = True
         for j in range(len(items) - i - 1):
             if items[j] > items[j + 1]:
                 items[j], items[j +1] = items[j+ 1], items[j]
+                already_sorted = False
+            if already_sorted:
+                break
     return items
