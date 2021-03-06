fc6047654bd07f938850527ab37ac5f96ffaabcd
davestroud <david@davidstroud.me>
2021-03-06 15:54:13 -0600

    correctly write bubble sort

--- Real_Python/Sorting/bubble_sort.py
+++ Real_Python/Sorting/bubble_sort.py
@@ -1,12 +1,12 @@
 def bubble_sort(items):
-    for i in range(len(items)):
-        already_sorted = True
-        for j in range(len(items) - i - 1):
-            if items[j] > items[j + 1]:
-                items[j], items[j +1] = items[j+ 1], items[j]
-                already_sorted = False
-            if already_sorted:
-                break
+    for i in range(len(items)): # Iterate through whole list
+        already_sorted = True # Set break condition
+        for j in range(len(items) - i - 1): # Maintain a growing section of the list that is sorted
+            if items[j] > items[j + 1]: # Swap all out of place items.
+                items[j], items[j + 1] = items[j + 1], items[j]
+                already_sorted = False # Set sorted to false if we needed to swap
+        if already_sorted:
+            break
     return items
 
 items = [1, 2, 5, 2, 3, 8, 1]
