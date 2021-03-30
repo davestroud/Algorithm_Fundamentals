fc35ed90b2f12cfe9908b7710764fa12855c5790
dstroud <david@davidstroud.me>
2021-03-30 09:25:09 -0500

    add longest substring

--- Facebook/Longest_Substring.py
+++ Facebook/Longest_Substring.py
@@ -1,23 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        chars = [0] * 128
-
-        left = right = 0
-
-        res = 0
-        while right < len(s):
-            r = s[right]
-            chars[ord(r)] += 1
-
-            while chars[ord(r)] > 1:
-                l = s[left]
-                chars[ord(l)] -= 1
-                left += 1
-
-            res = max(res, right - left + 1)
-
-            right += 1
-        return res
-    
+        n = len(s)
+        ans = 0
+        mp = {}
+        
+        i = 0
+        
+        for j in range(n):
+            if s[j] in mp:
+                i = max(mp[s[j]], i)
+            
+            ans = max(ans, j - i + 1)
+            mp[s[j]] = j + 1
+            
+        return ans
+        
 obj1 = Solution()
 print(obj1.lengthOfLongestSubstring("ABCABCBB"))
