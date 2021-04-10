ed568773aae8b876206dc5840e3c7bd7b87f8f59
dstroud <david@davidstroud.me>
2021-04-10 14:03:03 -0500

    add represenatives to LinkedList class

--- Facebook/Linked_Lists/Linked_List.py
+++ Facebook/Linked_Lists/Linked_List.py
@@ -10,6 +10,15 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None 
+        
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
 
         
 
