5797fa87b075c4efc4d8839dc5bd5336da6fdfa3
dstroud <david@davidstroud.me>
2021-04-10 14:00:19 -0500

    add representation of the objects

--- Facebook/Linked_Lists/Linked_List.py
+++ Facebook/Linked_Lists/Linked_List.py
@@ -1,9 +1,16 @@
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        
+    def __repr__(self):
+        return self.data
+        
+
+class LinkedList:
+    def __init__(self):
+        self.head = None 
+
+        
+
         
