e807d5f3505e16dd4f318559ac0ea9f664d13713
dstroud <david@davidstroud.me>
2021-04-10 13:57:16 -0500

    add linked list

---
+++ Facebook/Linked_Lists/Linked_List.py
@@ -0,0 +1,9 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
