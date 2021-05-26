11645792355034918e25476695db7b845b1db52a
dstroud <david@davidstroud.me>
2021-05-26 09:50:43 -0500

    add list of product elements

--- educative/Lists/List_Of_Products.py
+++ educative/Lists/List_Of_Products.py
@@ -1,2 +1,19 @@
 def find_product(lst):
-    pass
+    left = 1
+    product = []
+    for ele in lst:
+        product.append(left)
+        left = left * ele 
+    
+    right = 1
+    for i in range(len(lst)-1, -1, -1):
+        product[i] = product[i] * right
+        right = right * lst[i]
+    
+    return product 
+
+
+arr = [1,2,3,4]
+
+print(find_product(arr))
+
