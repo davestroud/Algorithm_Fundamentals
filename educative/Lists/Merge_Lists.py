1ebee81520c97a97616c8695476806802366378c
dstroud <david@davidstroud.me>
2021-05-26 08:54:28 -0500

    add list of products file

--- educative/Lists/Merge_Lists.py
+++ educative/Lists/Merge_Lists.py
@@ -1,11 +1,11 @@
 # My initial solution
 def merge_lists(lst1, lst2):
-    for x in list2:
-         list1.append(x)
-         list1.sort()
-    return list1 
+    for x in lst2:
+         lst1.append(x)
+         lst1.sort()
+    return lst1 
         
-list1 = [1,3,4,5]  
-list2 = [2,6,7,8]
+lst1 = [1,3,4,5]  
+lst2 = [2,6,7,8]
 
-print(merge_lists(list1,list2))
+print(merge_lists(lst1,lst2))
