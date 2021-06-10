481cde2f752a89b3a7491e87d41aa4ca79df1a0c
davestroud <jdstroud@smu.edu>
2021-06-10 17:45:36 +0000

    add notes

---
+++ Leet_Code/Arrays/string_to_int.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        # Read in and ignore whitespace
+
+        # Check is next char is -/+ 
+        # Read in if either
+
+        # Read in next char nutil next non-dig char
+        # or end is reached
+
+        # Convert dig to int ~ if no read == 0
+
+        # If int out of 32bit ~ clamp int
+
+        # Return int
+
